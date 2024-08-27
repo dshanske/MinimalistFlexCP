0fd6d867bd393cc5edcb8617718618f9b07453f8
Frank419 <hufang2007@icloud.com>
2024-08-27 13:01:57 +0800

    Added a default background image.

--- functions.php
+++ functions.php
@@ -4,7 +4,14 @@ if ( !defined( 'WPINC' ) ) {
 }
 
 function minimalistflex_add_supports() {
-	add_theme_support( 'custom-background' );
+	add_theme_support( 'custom-background', Array(
+		'default-image' => get_template_directory_uri() . '/defaults/bg.png',
+		'default-position-x' => 'center',
+		'default-position-y' => 'center',
+		'default-size' => 'cover',
+		'default-repeat' => 'no-repeat',
+		'default-attachment' => 'fixed'
+	) );
 	add_theme_support( 'title-tag' );
 	add_theme_support( 'automatic-feed-links' );
 	add_theme_support( "align-wide" );
