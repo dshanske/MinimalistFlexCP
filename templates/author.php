c1bbb0d50fd5d413d404f4c0b0fad244a80bdf27
David Shanske <david@shanske.com>
2024-11-29 16:28:24 +0000

    Add markup to author template

--- templates/author.php
+++ templates/author.php
@@ -6,12 +6,12 @@ if ( !defined( 'WPINC' ) ) {
 
 <?php $mf_userid = get_the_author_meta( 'ID' ) ?>
 <div class="singular-author">
-    <div class="author-card">
+    <div class="author-card h-card p-author">
         <div class="author-avatar">
             <?php echo get_avatar( $mf_userid, 80 ); ?>
         </div>
         <div class="author-description">
-            <a class="author-link" href="<?php echo esc_url( get_author_posts_url( $mf_userid ) ) ?>"><?php the_author() ?></a>
+            <a class="author-link u-url" href="<?php echo esc_url( get_author_posts_url( $mf_userid ) ) ?>"><span class="p-name"><?php the_author() ?></span></a>
             <p class="author-tagline"><?php the_author_meta( 'description' ) ?></p>
         </div>
     </div>
